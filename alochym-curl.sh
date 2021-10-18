298f64b31e5c185b3c327432b46faae5cd7de37a
Your Name <you@example.com>
2021-10-18 15:47:30 +0700

    alochym-curl.sh

--- alochym-curl.sh
+++ alochym-curl.sh
@@ -1,24 +1,24 @@
 urls="
-    localhost:4000/metrics?host=https://192.168.2.157
-    localhost:4000/metrics?host=https://192.168.2.158
-    localhost:4000/metrics?host=https://192.168.2.164
-    localhost:4000/metrics?host=https://192.168.2.165
-    localhost:4000/metrics?host=https://192.168.2.166
-    localhost:4000/metrics?host=https://192.168.2.167
-    localhost:4000/metrics?host=https://192.168.2.168
-    localhost:4000/metrics?host=https://192.168.2.169
-    localhost:4000/metrics?host=https://192.168.2.170
-    localhost:4000/metrics?host=https://192.168.2.171
-    localhost:4000/metrics?host=https://192.168.2.172
-    localhost:4000/metrics?host=https://192.168.2.173
-    localhost:4000/metrics?host=https://192.168.2.174
-    localhost:4000/metrics?host=https://192.168.2.175
-    localhost:4000/metrics?host=https://192.168.2.176
-    localhost:4000/metrics?host=https://192.168.2.177
-    localhost:4000/metrics?host=https://192.168.2.178
-    localhost:4000/metrics?host=https://192.168.2.179
-    localhost:4000/metrics?host=https://192.168.2.181
-    localhost:4000/metrics?host=https://192.168.2.182
+    42.118.242.143:4000/metrics?host=https://192.168.2.157
+    42.118.242.143:4000/metrics?host=https://192.168.2.158
+    42.118.242.143:4000/metrics?host=https://192.168.2.164
+    42.118.242.143:4000/metrics?host=https://192.168.2.165
+    42.118.242.143:4000/metrics?host=https://192.168.2.166
+    42.118.242.143:4000/metrics?host=https://192.168.2.167
+    42.118.242.143:4000/metrics?host=https://192.168.2.168
+    42.118.242.143:4000/metrics?host=https://192.168.2.169
+    42.118.242.143:4000/metrics?host=https://192.168.2.170
+    42.118.242.143:4000/metrics?host=https://192.168.2.171
+    42.118.242.143:4000/metrics?host=https://192.168.2.172
+    42.118.242.143:4000/metrics?host=https://192.168.2.173
+    42.118.242.143:4000/metrics?host=https://192.168.2.174
+    42.118.242.143:4000/metrics?host=https://192.168.2.175
+    42.118.242.143:4000/metrics?host=https://192.168.2.176
+    42.118.242.143:4000/metrics?host=https://192.168.2.177
+    42.118.242.143:4000/metrics?host=https://192.168.2.178
+    42.118.242.143:4000/metrics?host=https://192.168.2.179
+    42.118.242.143:4000/metrics?host=https://192.168.2.181
+    42.118.242.143:4000/metrics?host=https://192.168.2.182
 "
 for url in $urls; do
    # run the curl job in the background so we can start another job
